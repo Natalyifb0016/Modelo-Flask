a957603a05e1f53e4d8a4f34bb02c93eaa84f31b
Natalyifb0016 <natalydamaceno01@gmail.com>
2026-09-03 13:24:46 +0000

    projeto1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'chave-super-secretap                                                                  '
 
 @app.route("/")
 def login():
@@ -8,7 +9,9 @@ def login():
 
 @app.route("/painel")
 def painel():
-    return render_template("painel.html")
+   if 'usuario_nome' in session:
+    return render_template("painel.html",usuario_nome=session[usuario_nome])
+    return redirect('/')
 
 @app.route("/verificar", methods=['POST'])
 def verificar():
@@ -17,6 +20,17 @@ def verificar():
     
     print('Tentando login com: ', cpf,'/senha: ',senha)
 
+    if cpf == "12345678910" and senha =="BREU":
+        session['usuario_nome'] = "Gatos"
+        session['usuario_cpf'] = cpf
+        return redirect('/painel')
+
+    return redirect('/')
+
+@app.route('/sair')
+def sair():
+    session.pop('usuario_nome',None)
+    session.pop('usuario_cpf',None)
     return redirect('/')
 
 if __name__ == "__main__":
